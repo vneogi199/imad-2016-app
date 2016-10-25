094d1090dccc05786b22685258bbade2f40c3d72
Vinit Neogi <vneogi199@gmail.com>
2016-10-25 12:35:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/register.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'register.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
