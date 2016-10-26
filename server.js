408889db415421bd309740e70b96173139151143
Vinit Neogi <vneogi199@gmail.com>
2016-10-26 22:28:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
-
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
