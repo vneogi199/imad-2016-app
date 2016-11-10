c00e48c446604e4ee44e757c54e27640aade69b7
Vinit Neogi <vneogi199@gmail.com>
2016-11-10 15:20:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('test-db',function (req, res)   {
+app.get('/test-db',function (req, res)   {
     pool.query('SELECT * FROM test', function(err, result)  {
        if(err)  {
            res.status(500).send(err.toString());
