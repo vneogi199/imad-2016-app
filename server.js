8a1d8b75f60ab22b514b98d2f9ece015a7ef8e81
Vinit Neogi <vneogi199@gmail.com>
2016-11-11 15:02:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'vneogi199',
@@ -22,6 +23,17 @@ res.sendFile(path.join(__dirname, 'ui', 'test.php'));
 
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input, salt) {
+    //creation of hash
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function(req,res)    {
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
 });
 
 app.get('/test-db',function (req, res)   {
