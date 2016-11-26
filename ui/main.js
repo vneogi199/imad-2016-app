4ed831827fe283155bfebfdf6cd34394dadb303d
Vinit Neogi <vneogi199@gmail.com>
2016-11-26 13:35:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -73,6 +73,10 @@ function loadLoginForm () {
         // Make the request
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
+        if(username || password === '')  {
+            alert("Please enter values to continue");
+            return false;
+        }
         console.log(username);
         console.log(password);
         request.open('POST', '/create-user', true);
